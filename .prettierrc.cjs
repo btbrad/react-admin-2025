ec616ef37e36a69ed18a37bb30df757fa4409d63
btbrad <biantaobrad@gmail.com>
2025-11-14 17:50:48 +0800

    chore: prettier配置

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  trailingComma: 'none'
+}
