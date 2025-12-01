9289defc2bb71dad0e839995cb11567c781e9fae
btbrad <biantaobrad@gmail.com>
2025-12-01 10:29:48 +0800

    feat: 自定义主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './router/index.tsx'
 import './App.css'
+import { ConfigProvider } from 'antd'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ed6c00'
+        }
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  )
 }
 
 export default App
