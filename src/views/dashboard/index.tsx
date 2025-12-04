d5bea60b30606d32b281ef940debdd350c55a099
btbrad <biantaobrad@gmail.com>
2025-12-04 11:57:46 +0800

    feat: 控制台布局

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,9 @@
-import { Descriptions } from 'antd'
+import { Button, Card, Descriptions } from 'antd'
 import type { DescriptionsProps } from 'antd'
+import * as echarts from 'echarts'
 
 import styles from './index.module.less'
+import { useEffect } from 'react'
 
 const items: DescriptionsProps['items'] = [
   {
@@ -30,13 +32,160 @@ const items: DescriptionsProps['items'] = [
     children: '前端工程师'
   },
   {
-    key: '5',
+    key: '6',
     label: '部门',
     children: '大前端'
   }
 ]
 
 const Dashboard: React.FC = () => {
+  useEffect(() => {
+    const lineChartDom = document.getElementById('lineChart')
+    const lineChartsInstance = echarts.init(lineChartDom)
+    lineChartsInstance.setOption({
+      legend: {
+        data: ['订单数量', '流水'],
+        show: true,
+        top: '5%'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      grid: {
+        left: '5%',
+        right: '5%',
+        bottom: '10%',
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: '订单数量',
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          smooth: true
+        },
+        {
+          name: '流水',
+          data: [220, 332, 401, 534, 690, 730, 220],
+          type: 'line',
+          smooth: true
+        }
+      ]
+    })
+    const pieChartCityDom = document.getElementById('pieChartCity')
+    const pieChartsCityInstance = echarts.init(pieChartCityDom)
+    pieChartsCityInstance.setOption({
+      title: {
+        text: '司机城市分布',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: '司机数量',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: '北京' },
+            { value: 735, name: '上海' },
+            { value: 580, name: '天津' },
+            { value: 484, name: '重庆' },
+            { value: 300, name: '深圳' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    })
+    const pieChartAgeDom = document.getElementById('pieChartAge')
+    const pieChartsAgeInstance = echarts.init(pieChartAgeDom)
+    pieChartsAgeInstance.setOption({
+      title: {
+        text: '司机年龄分布',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        right: 'right'
+      },
+      roseType: 'area',
+      series: [
+        {
+          name: '司机数量',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: '20岁' },
+            { value: 735, name: '30岁' },
+            { value: 580, name: '40岁' },
+            { value: 484, name: '50岁' },
+            { value: 300, name: '60岁' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    })
+    const radarChartDom = document.getElementById('radarChart')
+    const radarChartsInstance = echarts.init(radarChartDom)
+    radarChartsInstance.setOption({
+      title: {
+        text: '司机模型诊断'
+      },
+      legend: {
+        data: ['司机模型诊断']
+      },
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: '服务态度' },
+          { name: '在线时长' },
+          { name: '接单率' },
+          { name: '评分' },
+          { name: '关注度' }
+        ]
+      },
+      series: [
+        {
+          name: '司机模型诊断',
+          type: 'radar',
+          data: [
+            {
+              value: [4200, 3000, 20000, 35000, 50000, 18000],
+              name: '司机模型诊断'
+            }
+          ]
+        }
+      ]
+    })
+  }, [])
+
   return (
     <div className={styles.dashboard}>
       <div className={styles.userInfo}>
@@ -60,6 +209,24 @@ const Dashboard: React.FC = () => {
           <div className='title'>开通城市</div>
           <div className={styles.data}>50座</div>
         </div>
+      </div>
+      <div className={styles.chart}>
+        <Card title='订单和流水走势图' extra={<Button type='primary'>刷新</Button>}>
+          <div id='lineChart' className={styles.itemChart}></div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='司机分布' extra={<Button type='primary'>刷新</Button>}>
+          <div className={styles.pieChart}>
+            <div id='pieChartCity' className={styles.itemChart}></div>
+            <div id='pieChartAge' className={styles.itemChart}></div>
+          </div>
+        </Card>
+      </div>
+      <div className={styles.chart}>
+        <Card title='模型诊断' extra={<Button type='primary'>刷新</Button>}>
+          <div id='radarChart' className={styles.itemChart}></div>
+        </Card>
       </div>
     </div>
   )
